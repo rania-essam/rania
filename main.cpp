4021ffe4267a23fb40a97075afe8e482d11d4eae
rania-essam <ranon6389@gmail.com>
2025-02-22 07:49:13 -0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include<cmath>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<stack>
+#include<list>
+#include<queue>
+#include<deque>
+#include<set>
+#include<map>
+#include <unordered_map>
+#include<iomanip>
+#include<bitset>
+#include <cstdlib>
+//#include<bits/stdc++.h>
+#define pb push_back
+#define ll long long
+#define X x.size()
+#define T int t;cin >> t; while(t--)
+//#define sort sort(x.begin(),x.end())
+#define mod 1000000007
+using namespace std;
+const double PI = 3.14159265358979323846;
+
+void fast() {
+    ios_base::sync_with_stdio(false);
+    cout.tie(NULL);
+    cin.tie(NULL);
+}
+
+
+int main() {
+    fast();
+    //freopen("","r",stdin);
+   int t; cin>>t;
+   while(t--)
+   {
+       int a,b; cin>>a>>b;
+       cout<<a-b<<'\n';
+   }
+   return 0;
+}
+
+
+
